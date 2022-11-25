73611c6876cd61445b883d2d40f66be38ba8194f
LucasZille <101298946+LucasZille@users.noreply.github.com>
2022-11-25 19:13:37 -0300

    Update Questão2.py

--- Questão2.py
+++ Questão2.py
@@ -3,15 +3,13 @@ def pilhasiguais(pilha1, pilha2):
     indice = 0;
     if len(pilha1) != len(pilha2):
         igual = False
-        
     while (indice < len(pilha1)):
         if pilha1[indice] == pilha2[indice]:
             indice += 1
         else:
             igual = False
             break
-
     if igual == True:
-        print('A pilha é igual')
+        print('A pilha é igual.')
     else:
-        print('A pilha não é igual')
+        print('A pilha não é igual.')
